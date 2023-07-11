f840df8ffcb3cd0a20090ea12f0b525d33b2d1b0
mmaeffert <m.maeffert@outlook.de>
2023-07-11 11:45:17 +0200

    added .env file to the docker

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -10,9 +10,6 @@ apt-get update && apt-get install -y \
 # Install dependencies using Composer
 composer install --optimize-autoloader
 
-# Set up Apache configuration
-a2enmod rewrite
-
 # Change ownership of directories
 chown -R www-data:www-data /var/www/html/
 
@@ -27,6 +24,16 @@ sed -i "s|DocumentRoot /var/www/html|DocumentRoot ${DOCUMENT_ROOT}|" ${APACHE_CO
 
 # Enable the Apache rewrite module
 a2enmod rewrite
+
+# # Generate the Application Key
+# php artisan key:generate --force
+
+# # Set the Application Key in the .env file
+# NEW_APP_KEY=$(php artisan key:generate --show)
+# sed -i "s/^\(APP_KEY\s*=\s*\).*\$/\1${NEW_APP_KEY}/" .env
+
+# # Clear the Application Cache
+# php artisan cache:clear
 
 # Start Apache in the foreground
 exec apache2-foreground
